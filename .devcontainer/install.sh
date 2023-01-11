60c337d475461555db0040b1cf219dfa6ac24b44
Iftach Naftaly <iftahnaf@gmail.com>
2023-01-11 13:14:40 +0000

    adding models to the simulation, solving ik for the robot in example script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+CITROS_MODEL_PATH=/workspaces/citros_example_robotic_arm/gazebo_models
+GAZEBO_MODEL_PATH=~/.gazebo/models/
+
+mkdir -p ${GAZEBO_MODEL_PATH}
+
+yes | cp -rf ${CITROS_MODEL_PATH}/foldable_table ${GAZEBO_MODEL_PATH}
+yes | cp -rf ${CITROS_MODEL_PATH}/person_standing ${GAZEBO_MODEL_PATH}
+
 echo "Welcome to CITROS Robotic Arm example!"
